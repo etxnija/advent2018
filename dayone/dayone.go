3296cbff7f0b7843ca29c1f1cc8a7d02ee623658
Nils Janson <nils.janson@klarna.com>
2018-12-13 14:41:06 +0100

    solved both

--- dayone/dayone.go
+++ dayone/dayone.go
@@ -12,36 +12,70 @@ func main() {
 	fmt.Println("HEllo")
 	args := os.Args
 	fmt.Println("input: ", args[1])
-	fmt.Println(FrequencyFromFile(args[1]))
+	sa := FrequencyFromFile(args[1])
+	ia, err := stringAtoIntA(sa)
+	if err != nil {
+		panic(err.Error)
+	}
+	fmt.Println(Frequency(ia))
+	fmt.Println("Collibate: ", Callibration(ia))
 }
 
 // Frequency is calculated
-func Frequency(in string) int {
-	sa := strings.Split(in, ",")
+func Frequency(in []int) int {
 	result := 0
-	for _, s := range sa {
-		i, err := strconv.Atoi(strings.TrimSpace(s))
-		if err != nil {
-			panic("not at int")
-		}
-		result += i
+	for _, d := range in {
+		result += d
 	}
 	return result
 }
 
-func FrequencyFromFile(file string) int {
+func Callibration(in []int) int {
 	result := 0
+	l := len(in)
+	m := make(map[int]int, l)
+	m[0] = 1
 
+	for i := 0; i < l*10000; i++ {
+		result += in[i%l]
+		if m[result] > 0 {
+			return result
+		}
+		m[result] = m[result] + 1
+
+	}
+	return 0
+}
+
+// FrequencyString is calculated
+func FrequencyString(in string) []int {
+	ia, err := stringAtoIntA(strings.Split(in, ","))
+	if err != nil {
+		return []int{}
+	}
+	return ia
+}
+
+func stringAtoIntA(sa []string) ([]int, error) {
+	ia := make([]int, len(sa))
+	for i := 0; i < len(sa); i++ {
+		d, err := strconv.Atoi(strings.TrimSpace(sa[i]))
+		if err != nil {
+			return []int{}, err
+		}
+		ia[i] = d
+	}
+	return ia, nil
+}
+
+func FrequencyFromFile(file string) []string {
+	sa := []string{}
 	fileHandle, _ := os.Open(file)
 	defer fileHandle.Close()
 	fileScanner := bufio.NewScanner(fileHandle)
 
 	for fileScanner.Scan() {
-		i, err := strconv.Atoi(strings.TrimSpace(fileScanner.Text()))
-		if err != nil {
-			panic("not at int")
-		}
-		result += i
+		sa = append(sa, fileScanner.Text())
 	}
-	return result
+	return sa
 }
--- dayone/dayone_test.go
+++ dayone/dayone_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestFrequency(t *testing.T) {
+func TestFrequencyString(t *testing.T) {
 
 	var tests = []struct {
 		in     string
@@ -17,7 +17,29 @@ func TestFrequency(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.in, func(t *testing.T) {
-			r := Frequency(tt.in)
+			r := Frequency(FrequencyString(tt.in))
+			if r != tt.result {
+				t.Errorf("got %d, but wanted %d", r, tt.result)
+			}
+		})
+	}
+}
+
+func TestCallibration(t *testing.T) {
+
+	var tests = []struct {
+		in     string
+		result int
+	}{
+		{"+1, -1", 0},
+		{"3, +3, +4, -2, -4", 10},
+		{"-6, +3, +8, +5, -6", 5},
+		{"+7, +7, -2, -7, -4", 14},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.in, func(t *testing.T) {
+			r := Callibration(FrequencyString(tt.in))
 			if r != tt.result {
 				t.Errorf("got %d, but wanted %d", r, tt.result)
 			}
